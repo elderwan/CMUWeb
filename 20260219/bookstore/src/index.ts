f68a52c37f7c093af20f42d003cb46225cd0d318
ElderWan <51625539+elderwan@users.noreply.github.com>
2026-02-19 15:31:33 +0700

    Add files via upload

---
+++ 20260219/bookstore/src/index.ts
@@ -0,0 +1,24 @@
+import express, { Request, Response } from "express";
+import path from "path";
+import { serveIndex, getAllBooks, addBookHandler, deleteBookHandler, searchBooksHandler } from "./controllers/bookController";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(process.cwd(), "public")));
+
+app.get("/", serveIndex);
+
+app.get("/books", getAllBooks);
+
+app.post("/books/add", addBookHandler);
+
+app.delete("/books/delete/:bookNo", deleteBookHandler);
+
+app.get("/books/search", searchBooksHandler);
+
+app.listen(PORT, () => {
+    console.log(`Server running: http://localhost:${PORT}`);
+});
